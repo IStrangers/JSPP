94c3501bcb423308b511caeac703fb06bccee577
xjh <qqq000qqq@aliyun.com>
2022-06-26 16:01:25 +0800

    20220626

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
-import {reactive} from './reactive/reactive';
+import {reactive} from "./reactive"
+import {updateHook} from "./hooks"
 
+updateHook(() => {
+    console.log(666)
+})
 const obj = reactive({a:1})
 obj.a = 666
 console.log(obj)
--- src/reactive/index.ts
+++ src/reactive/index.ts
@@ -1,10 +1,14 @@
+import {hooksContext} from "../hooks"
+
 function reactive<T extends object>(obj : T) : T {
     return new Proxy(obj,{
         get: (target,property,receiver) => {
             return Reflect.get(target,property,receiver)
         },
         set: (target,property,value,receiver) => {
-            return Reflect.set(target,property,value,receiver)
+            const val = Reflect.set(target,property,value,receiver)
+            hooksContext.updateHooks.forEach(hook => hook())
+            return val
         }
     });
 }
