80edb0a3df96500651e2f20170bcd70f07210e7c
qqq000qqq@aliyun.com <xqq1258460261>
2022-06-29 16:30:28 +0800

    20220629

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,30 @@
 import { reactive } from "./reactive"
 import { updateHook } from "./hooks"
-import { createVirtualNode, renderVirtualNode, NodeType } from "./dom/index"
+import { createVirtualNode, renderVirtualNode, NodeType } from "./dom"
 
 updateHook(() => {
     const node = createVirtualNode("div", NodeType.ELEMENT, null, null, [
         createVirtualNode("ul", NodeType.ELEMENT, null, null, [
-            createVirtualNode("li", NodeType.ELEMENT, null, [{name:"customAttr",value:"custom"},{name:"className",value:"JSPP li"},{name:"style",value:{background:"red"}},{name:"@click",value:()=> alert("click")}], [
+            createVirtualNode("li", NodeType.ELEMENT, null, [
+                { name: "customAttr", value: "custom" },
+                { name: "className", value: "JSPP li" },
+                { name: "style", value: { background: "red" } },
+                { name: "@click", value: () => alert("click") },
+                { name: "@mouseenter", value: () => alert("mouseenter") },
+            ], [
                 createVirtualNode(null, NodeType.TEXT, "123", null, null),
             ]),
-            createVirtualNode("li", NodeType.ELEMENT, null, [{name:"customAttr",value:"custom"},{name:"className",value:"JSPP li"},{name:"style",value:{background:"red"}},{name:"@click",value:()=> alert("click")}], [
+            createVirtualNode("li", NodeType.ELEMENT, null, [
+                { name: "customAttr", value: "custom" },
+                { name: "className", value: "JSPP li" },
+                { name: "style", value: { background: "red" } },
+                { name: "@click", value: () => alert("click") },
+                { name: "@mouseenter", value: () => alert("mouseenter") },
+            ], [
                 createVirtualNode(null, NodeType.NUMBER, "456", null, null),
             ])
         ]),
-        createVirtualNode(null,NodeType.NUMBER, "000", null, null)
+        createVirtualNode(null, NodeType.NUMBER, "000", null, null)
     ])
     renderVirtualNode(document.getElementById("app"), node)
 })
